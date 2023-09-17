024408db241e90a026ffb3546fce85d2d5e35cc1
northerose <roselinjimenez1@gmail.com>
2023-09-17 19:40:21 -0300

    cambiando nombre de la app registro por inicio

--- hospital/urls.py
+++ hospital/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registro/', include('registro.urls')),
+    path('inicio/', include('inicio.urls')),
 
 ]
