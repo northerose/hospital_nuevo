f975f89d7a73c71c06210e0f50a51e24299dce49
northerose <roselinjimenez1@gmail.com>
2023-09-17 21:27:43 -0300

    agregando filtro para que el título de la pestaña se vea en mayúscula

--- inicio/views.py
+++ inicio/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def inicio(request):
-    return render(request, 'inicio/inicio.html')
+    titulo = 'inicio'
+    return render(request, 'inicio/inicio.html', {'titulo': titulo})
 
