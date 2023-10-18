c8a96226dbff898db79c8d4fa1fbb69a97dd2c87
Roselin <84097205+northerose@users.noreply.github.com>
2023-10-18 20:42:42 -0300

    Revert "Revert "Dev carolina""

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-            path('', views.inicio, name='inicio')
-            ]
+    path('', views.inicio, name='inicio'),
+    path('contacto/', views.contacto, name='contacto')
+]
